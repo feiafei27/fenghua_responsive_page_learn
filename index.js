de0a94fc606f07631fd133999ff9daac2cc69242
付飞 <2543736927@qq.com>
2021-03-09 17:24:25 +0800

    f

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ require('./style.scss')
 
 const Glide = require('./libs/glide/glide.min')
 const anime = require('./libs/anime/anime.min')
+const Isotope = require('./libs/isotope/isotope.pkgd.min')
 
 
 
 
-// 实例化轮播
+// 轮播图
 const glide = new Glide(".glide")
 const captionsEl = document.querySelectorAll(".slide-caption")
 
@@ -32,3 +33,45 @@ glide.on("run.before", () => {
 })
 
 glide.mount()
+
+// 成功案例
+console.log(Isotope)
+const isotope = new Isotope(".cases", {
+  layoutMode: "fitRows",
+  itemSelector: ".case-item"
+})
+
+const filterBtns = document.querySelector(".filter-btns")
+filterBtns.addEventListener("click", e => {
+  let { target } = e
+  const filterOption = target.getAttribute("data-filter")
+  if(filterOption){
+    document.querySelectorAll(".filter-btn.active").forEach(btn => btn.classList.remove("active"))
+    target.classList.add("active")
+
+    isotope.arrange({filter: filterOption})
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
